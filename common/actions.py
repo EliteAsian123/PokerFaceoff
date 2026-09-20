d147ab9b5ee54a3539fee9668047d7a0416de592
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2026-09-20 13:29:16 -0400

    Create `Fold`, `Bet`, and `AllIn` player actions

---
+++ common/actions.py
@@ -0,0 +1,32 @@
+from typing import Literal
+
+class Fold:
+    type: Literal["fold"]
+
+    def __init__(self):
+        self.type = "fold"
+
+    def __repr__(self):
+        return "Fold"
+
+class Bet:
+    type: Literal["bet"]
+    up_to: int
+
+    def __init__(self, up_to: int):
+        self.type = "bet"
+        self.up_to = up_to
+
+    def __repr__(self):
+        return f"Bet(up_to={self.up_to})"
+
+class AllIn:
+    type: Literal["allIn"]
+
+    def __init__(self):
+        self.type = "allIn"
+
+    def __repr__(self):
+        return f"AllIn"
+
+Action = Fold | Bet | AllIn
